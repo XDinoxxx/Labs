2b45def2df30dd7c09b014d55b84ead6b0463b14
Роман Александрович <ra.roman88@yandex.ru>
2023-04-27 17:09:15 +0300

    add
    web - finish 5 lab

--- CM/lab4/lab4/lab4.cpp
+++ CM/lab4/lab4/lab4.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 const double eps = 0.001; // точность решения
-const int max_iter = 100; // максимальное число итераций
+const int max_iter = 1000; // максимальное число итераций
 
 // функции системы уравнений
 double f1(double x, double y) {
@@ -15,86 +15,83 @@ double f2(double x, double y) {
     return cos(x - 1) + y - 0.7;
 }
 
-// Якобиан системы уравнений
-void jacobian(double x, double y, double z, double& j11, double& j12, double& j13, double& j21, double& j22, double& j23, double& j31, double& j32, double& j33) {
-    j11 = 2;
-    j12 = z * sin(y * z);
-    j13 = y * sin(y * z);
-    j21 = -x / sqrt(1 - x * x);
-    j22 = -cos(y);
-    j23 = 0;
-    j31 = -exp(-x);
-    j32 = -0.5 * sin(y);
-    j33 = 1;
+// Частные производные
+double df1_dx(double x, double y) {
+    return 2;
 }
 
+double df1_dy(double x, double y) {
+    return cos(y);
+}
+
+double df2_dx(double x, double y) {
+    return -sin(x - 1);
+}
+
+double df2_dy(double x, double y) {
+    return 1;
+}
+
+
+
 void newtonMethod() {
-    double x = 0.01, y = 0.01; // начальное приближение
-    double x_prev, y_prev; // предыдущее приближение
-    int iter = 0; // число итераций
+    double x = 1, y = 1; // Начальное приближение
+    double eps = 0.001; // Точность решения
+    int n = 0; // Число итераций
+    double dx, dy; // Инкремент
+    double err; // Погрешность
 
     do {
-        x_prev = x;
-        y_prev = y;
+        double J = df1_dx(x, y) * df2_dy(x, y) - df2_dx(x, y) * df1_dy(x, y); // Вычисляем якобиан
+        dx = (-f2(x, y) * df1_dy(x, y) + f1(x, y) * df2_dy(x, y)) / J; // Вычисляем инкремент для x
+        dy = (f2(x, y) * df1_dx(x, y) - f1(x, y) * df2_dx(x, y)) / J; // Вычисляем инкремент для y
+        x += dx;
+        y += dy;
+        err = sqrt(pow(dx, 2) + pow(dy, 2)); // Вычисляем погрешность решения
+        n++;
+    } while (err >= eps);
 
-        // вычисление новых значений переменных
-        x = f1(x_prev, y_prev);
-        y = f2(x_prev, y_prev);
+    cout << "Решение системы нелинейных уравнений: " << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "Число итераций: " << n << endl;
+    cout << "Погрешность решения: " << err << endl;
+}
 
-        iter++;
-    } while (iter < max_iter && (abs(x - x_prev) > eps || abs(y - y_prev) > eps));
+void iterationMethod() {
+    double x = 1, y = 1; // Начальное приближение
+    double eps = 0.001; // Точность решения
+    int n = 0; // Число итераций
+    double nx, ny; // Новое приближение
+    double err; // Погрешность
 
-    // вывод результатов
-    cout << "Решение: x = " << x << ", y = " << y << endl;
-    cout << "Количество итераций: " << iter << endl;
+    do {
+        nx = (2 - sin(y)) / 2; // Вычисляем новое приближение для x
+        ny = 0.7 - cos(x - 1); // Вычисляем новое приближение для y
+        err = sqrt(pow(nx - x, 2) + pow(ny - y, 2)); // Вычисляем погрешность решения
+        x = nx;
+        y = ny;
+        n++;
+    } while (err >= eps);
 
-    // оценка погрешности решения
-    double err = max(abs(x - x_prev), abs(y - y_prev));
-    cout << "Погрешность: " << err << endl;
+    cout << "Решение системы нелинейных уравнений: " << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "Число итераций: " << n << endl;
+    cout << "Погрешность решения: " << err << endl;
 }
 
 int main() {
     setlocale(LC_ALL, "ru");
+
+    cout << "Метод Ньютона" << endl;
+
+    newtonMethod();
     
-    double x = 0.5, y = 0.5, z = 0.5; // начальное приближение
-    double x_prev, y_prev, z_prev; // предыдущее приближение
-    int iter = 0; // число итераций
+    cout << endl << "Итерационный метод" << endl;
 
-    do {
-        x_prev = x;
-        y_prev = y;
-        z_prev = z;
+    iterationMethod();
 
-        // вычисление Якобиана и вектора значений функций
-        double j11, j12, j13, j21, j22, j23, j31, j32, j33;
-        jacobian(x_prev, y_prev, z_prev, j11, j12, j13, j21, j22, j23, j31, j32, j33);
-        double f1_val = f1(x_prev, y_prev);
-        double f2_val = f2(x_prev, y_prev);
-
-        // решение системы линейных уравнений
-        double det = j11 * j22 * j33 + j12 * j23 * j31 + j13 * j21 * j32 - j13 * j22 * j31 - j12 * j21 * j33 - j11 * j23 * j32;
-        double dx = (j22 * j33 - j23 * j32) * f1_val + (j13 * j32 - j12 * j33) * f2_val + (j12 * j23 - j13 * j22) * f3_val;
-        double dy = (j23 * j31 - j21 * j33) * f1_val + (j11 * j33 - j13 * j31) * f2_val + (j13 * j21 - j11 * j23) * f3_val;
-        double dz = (j21 * j32 - j22 * j31) * f1_val + (j12 * j31 - j11 * j32) * f2_val + (j11 * j22 - j12 * j21) * f3_val;
-        dx /= det;
-        dy /= det;
-        dz /= det;
-
-        // вычисление новых значений переменных
-        x = x_prev - dx;
-        y = y_prev - dy;
-        z = z_prev - dz;
-
-        iter++;
-    } while (iter < max_iter && (abs(x - x_prev) > eps || abs(y - y_prev) > eps || abs(z - z_prev) > eps));
-
-    // вывод результатов
-    cout << "Solution: x = " << x << ", y = " << y << ", z = " << z << endl;
-    cout << "Number of iterations: " << iter << endl;
-
-    // оценка погрешности решения
-    double err = max(abs(x - x_prev), max(abs(y - y_prev), abs(z - z_prev)));
-    cout << "Error: " << err << endl;
 
     return 0;
 }
---
+++ CM/lab5/lab5/lab5.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+const double PI = 3.14159;
+
+// аналитическая функция
+double f(double x)
+{
+    return sin(2 * PI * x) / (1 + x);
+}
+
+// многочлен Лагранжа
+double lagrange(int n, const vector<double>& x, const vector<double>& y, double x0)
+{
+    double result = 0.0;
+
+    // вычисляем многочлен Лагранжа для заданной точки x0
+    for (int i = 0; i <= n; i++)
+    {
+        double li = 1.0;
+
+        for (int j = 0; j <= n; j++)
+        {
+            if (j != i)
+            {
+                li *= (x0 - x[j]) / (x[i] - x[j]);
+            }
+        }
+
+        result += y[i] * li;
+    }
+
+    return result;
+}
+
+// разделенные разности
+double divided_difference(int n, const vector<double>& x, const vector<double>& y, int i, int j)
+{
+    if (j == 0)
+    {
+        return y[i];
+    }
+
+    return (divided_difference(n, x, y, i + 1, j - 1) - divided_difference(n, x, y, i, j - 1)) / (x[i + j] - x[i]);
+}
+
+// многочлен Ньютона
+double newton(int n, const vector<double>& x, const vector<double>& y, double x0)
+{
+    double result = y[0];
+    double temp = 1.0;
+
+    // вычисляем многочлен Ньютона для заданной точки x0
+    for (int i = 1; i <= n; i++)
+    {
+        temp *= (x0 - x[i - 1]);
+        result += temp * divided_difference(i - 1, x, y, 0, i - 1);
+    }
+
+    return result;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double a = 0.0, b = 2.0, h = 0.1;
+    int n = (b - a) / h + 1;
+    vector<double> x(n), y(n);
+    vector<double> x0 = { 0.25, 1.0, 1.75 };
+
+    // получаем таблицу значений функции на отрезке [a, b] с шагом h
+    for (int i = 0; i < n; i++)
+    {
+        x[i] = a + i * h;
+        y[i] = f(x[i]);
+    }
+
+    // вычисляем значения функции в заданных точках с помощью многочлена Лагранжа
+    cout << "Значения функции в точках x:" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        double y0 = lagrange(4, x, y, x0[i]);
+        cout << "f(" << x0[i] << ") = " << y0 << endl;
+    }
+
+    cout << endl;
+
+    // вычисляем значения функции в заданных точках с помощью многочлена Ньютона
+    cout << "Значения функции в точках x:" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        double y0 = newton(4, x, y, x0[i]);
+        cout << "f(" << x0[i] << ") = " << y0 << endl;
+    }
+
+    return 0;
+}
