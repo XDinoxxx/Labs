2ed4950949ec3a4217a2d9ff90b28ba51b812d9b
Роман Александрович <ra.roman88@yandex.ru>
2023-04-29 21:08:06 +0300

    add OS

---
+++ OS/lab3.2creator/lab3.2creator/lab3.2creator.cpp
@@ -0,0 +1,28 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+void playSound() {
+    while (true) {
+        Beep(500, 500);
+        Sleep(1000);
+    }
+}
+
+void writeFile(HANDLE file) {
+    string message = "Hello, world!";
+    DWORD bytesWritten;
+    WriteFile(file, message.c_str(), (DWORD)message.length(), &bytesWritten, NULL);
+}
+
+int main() {
+    HANDLE soundThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)playSound, NULL, 0, NULL);
+    HANDLE file = CreateFile((LPCWSTR)L"D:\\Labs\\OS\\lab3.2user\\lab3.2user\\output.txt", GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    writeFile(file);
+    DuplicateHandle(GetCurrentProcess(), file, GetCurrentProcess(), &file, 0, FALSE, DUPLICATE_SAME_ACCESS);
+    CloseHandle(file);
+    return 0;
+}
---
+++ OS/lab3.2user/lab3.2user/lab3.2user.cpp
@@ -0,0 +1,25 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    HANDLE file = CreateFile((LPCWSTR)L"output.txt", GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (file == INVALID_HANDLE_VALUE) {
+        cout << "Unable to open file" << endl;
+        return 1;
+    }
+    char buffer[13];
+    DWORD bytesRead;
+    ReadFile(file, buffer, 13, &bytesRead, NULL);
+
+    string buf = buffer;
+    for (int i = 0; i < 13; i++) {
+        cout << buf[i];
+    }
+  
+    CloseHandle(file);
+    return 0;
+}
